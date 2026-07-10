657b3a933969fd6a87f9b9d8e05dcfefa610b8bf
Danil Kazakov <dani4upa586@gmail.com>
2026-07-10 18:57:59 +0900

    feat(constants): add centralized constants for messages, errors, and config

---
+++ Sources/swift-yt-downloader/Constants/Constants.swift
@@ -0,0 +1,43 @@
+enum Constants {
+
+    enum Bot {
+        static let start = "/start"
+        static let help = "/help"
+    }
+
+    enum Environment {
+        static let telegramBotToken = "TELEGRAM_BOT_TOKEN"
+    }
+
+    enum Messages {
+        static let help = "I can save audio from YouTube videos 🎬. Just send me a link to the YouTube video and after a few moments i will send you the audio."
+        static let hello = "Hi there! 👾\n\n\(Constants.Messages.help)"
+        static let wait = "Just a moment 👻!"
+        static let oops = "Oops! Something went wrong!"
+        static let notALink = "Send me a YouTube link 🔗"
+    }
+
+    enum Errors {
+        static let tokenNotSet = "Error: \(Constants.Environment.telegramBotToken) environment variable is not set.\n"
+        static let titleFetchFailed = "Failed to fetch video title"
+        static let ytDlpDownloadFailed = "yt-dlp download failed"
+        static let emptyOutput = "yt-dlp produced no output"
+    }
+
+    enum YouTube {
+        static let allowedHosts = [
+            "youtube.com",
+            "www.youtube.com",
+            "m.youtube.com",
+            "youtu.be"
+        ]
+
+        static let subdomainSuffix = ".youtube.com"
+        static let ytdlp = "yt-dlp"
+        static let bestAudioFormat = "bestaudio"
+        static let outputToStdout = "-"
+        static let printTitle = "title"
+        static let audioExtension = "m4a"
+        static let maxOutputBytes = 50 * 1024 * 1024
+    }
+}
